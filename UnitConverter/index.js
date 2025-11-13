40f823173d7d42b5ce1e2d80bb3e6197b3feb111
Mikiyas Batu <127264981+miki2123j@users.noreply.github.com>
2025-11-12 23:16:59 -0500

    Add files via upload

---
+++ UnitConverter/index.js
@@ -0,0 +1,62 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const inputEl = document.getElementById("input-el")
+const convertBtn = document.getElementById("convert-btn")
+const len = document.getElementById("length")
+const vol = document.getElementById("volume")
+const mass = document.getElementById("mass")
+
+
+convertBtn.addEventListener("click", function(){
+    render()
+})
+
+function render(){
+    let meterToFeetVal = meterToFeet()
+    let feetToMeterVal = feetToMeter()
+    let literToGallonsVal = literToGallons()
+    let gallonsToLitersVal = gallonsToLiters()
+    let kilogramToPoundVal = kilogramToPound()
+    let poundToKilogramVal = poundToKilogram()
+
+    let lenTemp = `${inputEl.value} meters = ${meterToFeetVal} feet | ${inputEl.value} feet = ${feetToMeterVal} meters`
+    let volTemp = `${inputEl.value} liters = ${literToGallonsVal} gallons | ${inputEl.value} gallons = ${gallonsToLitersVal} liters`
+    let massTemp = `${inputEl.value} kilos = ${kilogramToPoundVal} pounds | ${inputEl.value} pounds = ${poundToKilogramVal} kilos`
+    len.textContent = lenTemp
+    vol.textContent = volTemp
+    mass.textContent = massTemp
+}
+
+function meterToFeet(){
+    let output = Number(inputEl.value) * 3.281
+    return output.toFixed(3)
+}
+
+function feetToMeter(){
+    let output = Number(inputEl.value) / 3.281
+    return output.toFixed(3)
+}
+
+function literToGallons(){
+    let output = Number(inputEl.value) * 0.264
+    return output.toFixed(3)
+}
+
+function gallonsToLiters(){
+    let output = Number(inputEl.value) / 0.264
+    return output.toFixed(3)
+}
+
+function kilogramToPound(){
+    let output = Number(inputEl.value) * 2.204
+    return output.toFixed(3)
+}
+
+function poundToKilogram(){
+    let output = Number(inputEl.value) / 2.204
+    return output.toFixed(3)
+}
