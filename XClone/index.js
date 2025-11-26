281e2d2cd50ebaf5aea618217b630ab4e10a588d
Mikiyas Batu <127264981+miki2123j@users.noreply.github.com>
2025-11-26 14:11:43 -0500

    Render tweets feed and fix input value typo
    Added a function to generate and render the tweets feed from tweetsData. Fixed a typo in the tweet input value access and initialized feed rendering on page load.

--- XClone/index.js
+++ XClone/index.js
@@ -1,8 +1,43 @@
-import { tweetsData } from "./data/data.js"
-
+import { tweetsData } from './data/data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
+const feed = document.getElementById('feed')
 
 tweetBtn.addEventListener('click', function(){
-    console.log(tweetInput.vlaue)
+    console.log(tweetInput.value)
 })
+
+function getFeedHtml(){
+    let feedHtml = ``
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>`
+    })
+    return feedHtml
+}
+
+
+function render(){
+    feed.innerHTML = getFeedHtml()
+}
+
+render()
